5e7ff75a251e6608aa729e6115e9f0a24c4b1500
dev.jun28 <hsj9360@gmail.com>
2022-09-14 03:52:44 +0900

    feat(App): add home router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Home from '@/pages/home/Home';
+
 import Detail from './pages/detail/Detail';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<div>home</div>} />
+      <Route path="/" element={<Home />} />
       <Route path="/issue/:issueNumber" element={<Detail />} />
     </Routes>
   </BrowserRouter>
