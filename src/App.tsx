e4ee88ad0d70975edb2a12a1a83a1dce505ec6d1
owen <cockyblind@gmail.com>
2022-09-14 02:57:23 +0900

    Feat: 이슈 상세 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Detail from './pages/detail/Detail';
+
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<div>home</div>} />
+      <Route path="/issue/:issueNumber" element={<Detail />} />
     </Routes>
   </BrowserRouter>
 );
