126961fc09aa985b4e49d2291ceb5b714d266609
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-14 19:24:46 +0900

    feat: context api 설정 추가
    - axios instance 추가
    - getIssueApi, getIssueDetailApi 추가

--- src/store/api-context.tsx
+++ src/store/api-context.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { createContext, useEffect, useState } from 'react';
 
-import { IssueContextType, Props } from '@/@types/issue';
+import { IssueContextType, Props } from '@/types';
 
 const IssueStateContext = createContext({} as IssueContextType);
 
@@ -10,31 +10,46 @@ export const IssueProvider: React.FC<Props> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const getIssueData = async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(`${process.env.REACT_APP_API_HOST}`, {
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-        },
-      });
-      setIssueData(response.data);
-    } catch (err) {
-      setError(true);
-      console.log(err);
-    }
-    setIsLoading(false);
+  const BASE_URL = process.env.REACT_APP_API_HOST;
+
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      Accept: 'application/vnd.github+json',
+      Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+    },
+    paramsSerializer: (params) => {
+      return new URLSearchParams(params).toString();
+    },
+    timeout: 1000,
+  });
+
+  const api = {
+    getIssueApi: (config?: AxiosRequestConfig) =>
+      axiosInstance.get(`${BASE_URL}`, config).then((response) => response.data),
+    getIssueDetailApi: (issueNumber: number) =>
+      axiosInstance.get(`${BASE_URL}/${issueNumber}`).then((response) => response.data),
   };
-  console.log(issueData);
 
   useEffect(() => {
-    getIssueData();
+    (async () => {
+      setIsLoading(true);
+      try {
+        const response = await api.getIssueApi();
+        setIssueData(response);
+      } catch (err) {
+        setError(true);
+        console.error(err);
+      }
+      setIsLoading(false);
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // eslint-disable-next-line react/jsx-no-constructed-context-values
-  return <IssueStateContext.Provider value={{ issueData, error, isLoading }}>{children}</IssueStateContext.Provider>;
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <IssueStateContext.Provider value={{ api, issueData, error, isLoading }}>{children}</IssueStateContext.Provider>
+  );
 };
 
 export default IssueStateContext;
