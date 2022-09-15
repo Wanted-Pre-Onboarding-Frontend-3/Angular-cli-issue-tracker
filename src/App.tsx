ca435d7ad900a6f72b26249d9c7e56baf4482a0c
woowsnu <woowsnu@gmail.com>
2022-09-15 10:01:30 +0900

    feat: 리스트화면 css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/issue/:issueNumber" element={<Detail />} />
-          <Route path="/" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </IssueProvider>
